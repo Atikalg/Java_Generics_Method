3cd61445bc39da6a49bfb6534992cf8f3b1c528a
atikalg <who.atik@gmail.com>
2022-04-21 21:39:57 +0530

    Added : Welcome to print array program

---
+++ PrintArray.java
@@ -0,0 +1,11 @@
+package com.bridgelab.Assignment_13;
+
+/**
+ * @author atik
+ * Program to print array using generic method 
+ */
+public class PrintArray {
+	public static void main(String[] args) {
+		System.out.println("------------------- Welcome to Generic demo ------------------- ");
+	}
+}
